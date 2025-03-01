efeb8bc64f471a62206a2fca5def54ee67eed955
dani <danifromecuador@gmail.com>
2025-03-01 15:31:24 -0500

    create index action to show all the posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,21 @@
 class PostsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  def index
+    @new_post = Post.new
+    @all_posts = Post.all.order(:created_at).reverse
+    pp "##############################"
+    pp @all_posts
+  end
+  
+  def create
+    @new_post = Post.new(post_params)
+    redirect_to root_path if @new_post.save
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:comment)
+  end
+
 end
